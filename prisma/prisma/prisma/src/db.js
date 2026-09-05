eb3f35b68f2f8379b7c7ba3cde66fe384363e0c3
disuzaadesi-alt <disuzaadesi@gmail.com>
2026-09-05 07:18:12 +0530

    Delete prisma/prisma/prisma/src directory

--- prisma/prisma/prisma/src/db.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-
-// Reuse a single Prisma client across the app (avoids connection exhaustion)
-const prisma = new PrismaClient();
-
-module.exports = prisma;
